06a0c0df13df88c7d86cf6f8beaa9025aba3c11d
antrax <antrax2013@hotmail.com>
2020-12-06 17:45:26 +0100

    Feature 2 US 2.1

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const http = require('http');
-const WebSocket = require('ws');
+const http = (process.env.NODE_ENV === 'production' ) ? require('https'):require('http');
+const WebSocket = (process.env.NODE_ENV === 'production' ) ? require('wss'):require('ws');
 const jwt = require('jsonwebtoken');
  
 const port = process.env.PORT || 4913;
@@ -9,34 +9,48 @@ const server = http.createServer(app);
 
 server.version ='v.0.1.3';
 
-const wss = new WebSocket.Server({ server })
+const wss = new WebSocket.Server({ server });
 //const wss = new WebSocket.Server({port: port })
-//https://atomrace.com/creer-serveur-websocket-socket-io/
+//https://atomrace.com/creer-serveur-websocket-socket-io
 
-
-wss.newtoken = () => {
-  this.extra = {
-    token:jwt.sign({ userId: "chimere" }, 'RANDOM_TOKEN_SECRET', { expiresIn: '1h' }),
+wss.newtoken = (room) => {
+  return {
+    token:jwt.sign({ userId: "chimere"+room??"" }, 'RANDOM_TOKEN_SECRET', { expiresIn: '1h' }),
     date : new Date()
   }
 }
-wss.getTokenMessage = () => {
+
+wss.getTokenMessage = (extra) => {
   return JSON.stringify({
     name:"systeme",
     content : "",
     type: 6,
-    extra: this.extra
+    extra: extra
   })
 }
-wss.newtoken()
 
+wss.rooms = [];
 
+changeRoom = (socket, server) => {
+  socket.isAlive = false;
+  server.rooms.find(room => { room.users = room.users.filter(user => user !== socket)});
+}
+
+close = (socket, server) => {
+  socket.isAlive = false;
+  server.rooms.find(room => { room.users = room.users.filter(user => user !== socket)});
+  socket.terminate();
+}
 
 app.get('*', function(req, res, next) {
   return res.send('Hello World!');
 });
 
 wss.on('connection', function connection(ws) {
+
+  ws.isAlive = true;
+  ws.name="";
+  ws.roomName="";
 
   console.log('connection');
 
@@ -46,10 +60,7 @@ wss.on('connection', function connection(ws) {
 
   server.on('upgrade', async function upgrade(request, socket, head) {
     console.log('upgrade'/*,request, socket, head*/);
-  });
-
-  ws.isAlive = true;
-  ws.name="";
+  }); 
 
   ws.on('pong', () => {
     //console.log("pong")
@@ -57,9 +68,11 @@ wss.on('connection', function connection(ws) {
   });
 
   ws.on('close', () => {
-    console.log("close")
+    /*console.log("close");
     ws.isAlive = false;
-    ws.terminate();
+    wss.rooms.find(room => { room.users = room.users.filter(user => user !== ws)});
+    ws.terminate();*/
+    close(ws,wss)
   });
 
   server.on('open', function open() {
@@ -75,44 +88,80 @@ wss.on('connection', function connection(ws) {
   })
 
   ws.on('message', function incoming(data) {
-    let json = JSON.parse(data);
-    console.log('message',json)
+    let message = JSON.parse(data);
+    console.log('message',message)
+    
+    if(message.type === 5) {
+      changeRoom(ws,wss)
+    }
+    else if(message.type === 4) {
+      
+      ws.name = message.name;
+      ws.room = message.room;
 
-    wss.clients.forEach(function each(client) {
-      if (client !== ws && client.readyState === WebSocket.OPEN) {
-        client.send(data);
-      }
-      else {
-        if(!(data.extra!=undefined && data.extra.token!=undefined)) {
-          client.send(wss.getTokenMessage());
+      if(wss.rooms.filter(r=> r.name == message.room).length === 0)
+      {
+        let room = {
+          name : message.room,
+          users : [],
+          token : wss.newtoken()
         }
+        room.users.push(ws);
+
+        wss.rooms = [
+            room
+          , ...wss.rooms
+        ]
       }
+      else wss.rooms.find(r=> r.name ==message.room).users.push(ws)
+    }
+    
+
+    wss.rooms.filter(r=> r.name == message.room).forEach(room => 
+    { 
+      room.users.forEach(user => 
+      {
+        if (user !== ws && user.readyState === WebSocket.OPEN) 
+        {
+          user.send(data);
+        }
+        else 
+        {
+          if(!(data.extra!=undefined && data.extra.token!=undefined)) 
+          //if(message.type === 4) {
+            user.send(wss.getTokenMessage(room.token));
+        }
+      })
     })
   })
 })
 
 
 //renew token
-setInterval(() => {
-  //console.log("renew token")
-  wss.newtoken()
+setInterval(() => {  
   var i = 0;
-  wss.clients.forEach((client) => {     
+
+  wss.rooms.map(room => {
+    room.token = wss.newtoken(room.name);
+
+    room.users.map(user => {
+      console.log("room:"+room.name,"user:"+user.name/*,"token:"+room.token.token*/)
       i++;
-      if (!client.isAlive) {
+      if (!user.isAlive) {
         //console.log(" ********* is dead:");
-        return client.terminate();
+        return user.terminate();
       }
       else {
         //console.log(" ********* is alive ");
-        client.send(wss.getTokenMessage());
+        user.send(wss.getTokenMessage(room.token));
       }
       
-      client.isAlive = false;
-      client.ping(null, false, true);
-  });
-  console.log(i/2)
-}, 10000);
+      user.isAlive = false;
+      user.ping(null, false, true);
+    })
+  })
+  //console.log(i/2)
+}, 30000);
 
 server.listen(port, ()=> {
   console.log(`Server is listening on ${port}!`)
